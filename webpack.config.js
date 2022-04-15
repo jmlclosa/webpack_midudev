e5ce346efba5908a6a84f81a05f2eaae3f62716f
jlopez <juanma.lopezclosa@plexus.es>
2022-04-15 21:43:11 +0200

    Cleaning webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,23 @@
-// const path = require('path')
+const rulesForJavascript = {
+    test: /\.js$/,
+    loader: 'babel-loader',
+    options: {
+        presets: [
+            [
+                '@babel/preset-react',
+                {
+                    runtime: 'automatic'
+                }
+            ]
+        ]
+    }
+};
+const rules = [rulesForJavascript]
 
 module.exports = {
     // entry: './src/indexjs',
     // output: {
     //     path: path.resolve(__dirname, 'build')
     // }
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                options: {
-                    presets: [
-                        [
-                            '@babel/preset-react',
-                            {
-                                runtime: 'automatic'
-                            }
-                        ]
-                    ]
-                }
-            }
-        ]
-    }
+    module: {rules}
 }
